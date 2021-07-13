7d73d8dec58d69b397caf0f95e49beaa65e9e4a9
Oliver Tosky <oliver.tosky@360i.com>
2021-07-13 14:33:17 -0400

    add helper script for copying obsidian vault

---
+++ scripts/init_repo_from_vault.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Copies notes from an Obsidian vault to 11ty project structure.
+
+clear_example_notes() {
+  rm -rf ./notes/*.md
+}
+
+copy_notes_from_vault() {
+  find "$1" -name \*.md -exec cp {} ./notes \;
+}
+
+move_index_to_repo_root() {
+  mv ./notes/index.md ./
+}
+
+main() {
+  clear_example_notes
+  copy_notes_from_vault "$1"
+  move_index_to_repo_root
+}
+
+main "$1"
